efe16caf8a10a503ad4adcffdef335e38b282b2f
Riemer Palstra <riemer@palstra.com>
2025-08-02 19:08:45 +0200

    Edit Cypress E2E rules and enable it again.

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -3,14 +3,7 @@ describe('empty spec', () => {
     cy.visit('/')
   })
   it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Hugo project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+    cy.get('h1')
+    .contains('Ryan Palstra');
   })
 })
